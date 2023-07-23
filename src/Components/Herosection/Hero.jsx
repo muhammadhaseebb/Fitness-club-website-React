8632947641bf9275cb0eaa4e71f6159fdf3176cc
Muhammad Haseeb <104450111+muhammadhaseebb@users.noreply.github.com>
2023-07-23 23:34:06 +0500

    hero section in progress

---
+++ src/Components/Herosection/Hero.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Hero.css'
+import Header from '../Header/Header';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="left">
+           <Header/>
+           <div className="the-best-ad">
+            <div>
+            </div>
+                <span>The best fitness club in the town</span>
+           </div>
+
+           <div className="hero-text">
+            <div>
+                <span className='stroke-text'>Shape</span>
+                <span>Your</span>
+            </div>
+
+            <div>
+                <span>Ideal body</span>
+            </div>
+            <div >
+                <span>
+
+                In here we will help you to shape and build your ideal body and live up your life to fullest
+                </span>
+            </div>
+           </div>
+
+           <div className="figures">
+            <div>
+                <span>+140</span>
+                <span>Expert Coaches</span>
+            </div>
+            <div>
+                <span>+978</span>
+                <span>Members Joined</span>
+            </div>
+            <div>
+                <span>+50</span>
+                <span>Fitness Programs</span>
+            </div>
+            
+            
+           </div>
+        </div>
+        <div className="right">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
