8d08e51a2b83be43d3f43b8610f9023e96e7a212
Muhammad Haseeb <104450111+muhammadhaseebb@users.noreply.github.com>
2023-07-28 16:15:53 +0500

    final

--- src/Components/Herosection/Hero.jsx
+++ src/Components/Herosection/Hero.jsx
@@ -6,6 +6,7 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
@@ -44,15 +45,21 @@ const Hero = () => {
 
         <div className="figures">
           <div>
-            <span>+140</span>
+            <span>
+              <NumberCounter end={140} start={100} delay="4" preFix="+" />
+            </span>
             <span>Expert Coaches</span>
           </div>
           <div>
-            <span>+978</span>
+            <span>
+              <NumberCounter end={978} start={900} delay="4" preFix="+" />
+            </span>
             <span>Members Joined</span>
           </div>
           <div>
-            <span>+50</span>
+            <span>
+              <NumberCounter end={50} start={10} delay="4" preFix="+" />
+            </span>
             <span>Fitness Programs</span>
           </div>
         </div>
